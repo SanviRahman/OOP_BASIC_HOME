cf873a98c27d512749933d4c48e184f9f209e3a0
Sanvi Rahman <121854279+SanviRahman@users.noreply.github.com>
2025-05-12 23:44:46 +0600

    2nd commit

--- constructor.php
+++ constructor.php
@@ -3,20 +3,28 @@
     class Student
     {
         public $name;
+        public $roll;
 
-        public function __construct()
+        public function __construct($name, $roll)
         {
-            $this->name="Sanvi Rahman";
+            $this->name= $name;
+            $this->roll=$roll;
         }
 
 
         public function display()
         {
-            echo "Name is {$this->name}";
+            echo "Name is {$this->name}". PHP_EOL;
+            echo "Roll is {$this->roll}".PHP_EOL;
+        }
+
+        public function __destruct()
+        {
+            echo "Hello! I am from Destructor Class";
         }
 
     }
 
-    $myObj= new Student();
+    $myObj= new Student("Sanvi Rahman",10);
     $myObj->display();
 ?>
